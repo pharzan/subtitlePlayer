79884c9c0901d1bd88dfb8160bdd163026d289a3
pharzan <pharzan@outlook.com>
2015-10-22 16:31:32 +0300

    caption reader and caption object creator function

--- subtitlePlayer.js
+++ subtitlePlayer.js
@@ -3,16 +3,52 @@
  */
 
 var subtitlePlayer = function () {
+    var playerObj = this;
 
     this.vm = {
         content: '"1\\n00:00:07,031 --> 00:00:09,903\\nSadness is part of the human experience,\\n\\n2\\n00:00:09,903 --> 00:00:12,511\\nbut for centuries there has \\nbeen vast disagreement\\n\\n3\\n00:00:12,511 --> 00:00:17,377\\nover what exactly it is and what,\\nif anything, to do about it.\\n\\n4\\n00:00:17,377 --> 00:00:19,085\\nIn its simplest terms,\\n\\n5\\n00:00:19,085 --> 00:00:20,723\\nsadness is often thought of\\n\\n6\\n00:00:20,723 --> 00:00:23,966\\nas the natural reaction \\nto a difficult situation.\\n\\n7\\n00:00:23,966 --> 00:00:27,843\\nYou feel sad when a friend moves away\\nor when a pet dies.\\n\\n8\\n00:00:27,843 --> 00:00:28,927\\nWhen a friend says, \\"I\'m sad,\\"\\n\\n9\\n00:00:29,683 --> 00:00:31,765\\nyou often respond by asking,\\n\\"What happened?\\"\\n\\n10\\n00:00:32,503 --> 00:00:37,064\\nBut your assumption that sadness\\nhas an external cause outside the self\\n\\n11\\n00:00:37,064 --> 00:00:39,711\\nis a relatively new idea.\\n\\n12\\n00:00:39,711 --> 00:00:42,478\\nAncient Greek doctors didn\'t\\nview sadness that way.\\n\\n13\\n00:00:42,478 --> 00:00:46,256\\nThey believed it was a dark fluid\\ninside the body.\\n\\n14\\n00:00:46,256 --> 00:00:48,288\\nAccording to their humoral system,\\n\\n15\\n00:00:48,576 --> 00:00:53,383\\nthe human body and soul were controlled\\nby four fluids, known as humors,\\n\\n16\\n00:00:53,383 --> 00:00:58,155\\nand their balance directly influenced\\na person\'s health and temperament.\\n\\n17\\n00:00:58,155 --> 00:01:00,238\\nMelancholia comes from\\nmelaina kole,\\n\\n18\\n00:01:00,985 --> 00:01:05,644\\nthe word for black bile,\\nthe humor believed to cause sadness.\\n\\n19\\n00:01:05,644 --> 00:01:08,270\\nBy changing your diet\\nand through medical practices,\\n\\n20\\n00:01:08,027 --> 00:01:10,363\\nyou could bring your humors\\ninto balance.\\n\\n21\\n00:01:10,606 --> 00:01:13,628\\nEven though we now know\\nmuch more about the systems\\n\\n22\\n00:01:13,628 --> 00:01:15,351\\nthat govern the human body,\\n\\n23\\n00:01:15,351 --> 00:01:17,210\\nthese Greek ideas about sadness\\n\\n24\\n00:01:17,021 --> 00:01:18,628\\nresonate with current views,\\n\\n25\\n00:01:18,817 --> 00:01:21,506\\nnot on the sadness we all\\noccasionally feel,\\n\\n26\\n00:01:21,506 --> 00:01:23,750\\nbut on clinical depression.\\n\\n27\\n00:01:23,075 --> 00:01:25,411\\nDoctors believe that certain \\nkinds of long-term,\\n\\n28\\n00:01:26,086 --> 00:01:32,211\\nunexplained emotional states are at least\\npartially related to brain chemistry,\\n\\n29\\n00:01:32,211 --> 00:01:35,948\\nthe balance of various chemicals \\npresent inside the brain.\\n\\n30\\n00:01:35,948 --> 00:01:36,977\\nLike the Greek system,\\n\\n31\\n00:01:37,238 --> 00:01:39,932\\nchanging the balance of these chemicals\\ncan deeply alter\\n\\n32\\n00:01:39,932 --> 00:01:44,148\\nhow we respond to even extremely\\ndifficult circumstances.\\n\\n33\\n00:01:44,148 --> 00:01:46,921\\nThere\'s also a long tradition\\nof attempting to discern\\n\\n34\\n00:01:46,921 --> 00:01:48,020\\nthe value of sadness,\\n\\n35\\n00:01:48,911 --> 00:01:50,175\\nand in that discussion,\\n\\n36\\n00:01:50,175 --> 00:01:52,752\\nyou\'ll find a strong argument\\nthat sadness is not only\\n\\n37\\n00:01:52,752 --> 00:01:56,438\\nan inevitable part of life\\nbut an essential one.\\n\\n38\\n00:01:56,438 --> 00:01:58,237\\nIf you\'ve never felt melancholy,\\n\\n39\\n00:01:58,237 --> 00:02:01,795\\nyou\'ve missed out on part of\\nwhat it means to be human.\\n\\n40\\n00:02:01,795 --> 00:02:05,840\\nMany thinkers contend that melancholy\\nis necessary in gaining wisdom.\\n\\n41\\n00:02:06,245 --> 00:02:08,798\\nRobert Burton, born in 1577,\\n\\n42\\n00:02:08,798 --> 00:02:13,176\\nspent his life studying the causes\\nand experience of sadness.\\n\\n43\\n00:02:13,176 --> 00:02:16,171\\nIn his masterpiece\\n\\"The Anatomy of Melancholy,\\"\\n\\n44\\n00:02:16,171 --> 00:02:20,909\\nBurton wrote, \\"He that increaseth wisdom\\nincreaseth sorrow.\\"\\n\\n45\\n00:02:20,909 --> 00:02:23,758\\nThe Romantic poets of \\nthe early 19th century\\n\\n46\\n00:02:23,758 --> 00:02:29,016\\nbelieved melancholy allows us to more\\ndeeply understand other profound emotions,\\n\\n47\\n00:02:29,016 --> 00:02:30,603\\nlike beauty and joy.\\n\\n48\\n00:02:30,603 --> 00:02:34,607\\nTo understand the sadness of the trees\\nlosing their leaves in the fall\\n\\n49\\n00:02:34,643 --> 00:02:40,150\\nis to more fully understand the cycle\\nof life that brings flowers in the spring.\\n\\n50\\n00:02:40,015 --> 00:02:45,366\\nBut wisdom and emotional intelligence seem\\npretty high on the hierarchy of needs.\\n\\n51\\n00:02:45,501 --> 00:02:48,646\\nDoes sadness have value on\\na more basic, tangible,\\n\\n52\\n00:02:48,646 --> 00:02:51,252\\nmaybe even evolutionary level?\\n\\n53\\n00:02:51,252 --> 00:02:54,065\\nScientists think that crying\\nand feeling withdrawn\\n\\n54\\n00:02:54,065 --> 00:02:58,437\\nis what originally helped our\\nancestors secure social bonds\\n\\n55\\n00:02:58,437 --> 00:03:01,073\\nand helped them get the support they needed.\\n\\n56\\n00:03:01,073 --> 00:03:05,335\\nSadness, as opposed to anger or violence,\\nwas an expression of suffering\\n\\n57\\n00:03:05,335 --> 00:03:09,188\\nthat could immediately bring people closer\\nto the suffering person,\\n\\n58\\n00:03:09,188 --> 00:03:13,545\\nand this helped both the person\\nand the larger community to thrive.\\n\\n59\\n00:03:13,545 --> 00:03:16,606\\nPerhaps sadness helped generate\\nthe unity we needed to survive,\\n\\n60\\n00:03:16,606 --> 00:03:20,320\\nbut many have wondered whether\\nthe suffering felt by others\\n\\n61\\n00:03:20,032 --> 00:03:23,779\\nis anything like the suffering\\nwe experience ourselves.\\n\\n62\\n00:03:24,067 --> 00:03:25,864\\nThe poet Emily Dickinson wrote,\\n\\n63\\n00:03:25,864 --> 00:03:30,396\\n\\"I measure every Grief I meet\\nWith narrow, probing Eyes -\\n\\n64\\n00:03:30,396 --> 00:03:35,487\\nI wonder if it weighs like MIne -\\nOr has an Easier size.\\"\\n\\n65\\n00:03:35,487 --> 00:03:36,998\\nAnd in the 20th century,\\n\\n\\n66\\n00:03:36,998 --> 00:03:39,609\\nmedical anthropologists,\\nlike Arthur Kleinman,\\n\\n67\\n00:03:39,609 --> 00:03:42,853\\ngathered evidence from the way\\npeople talk about pain\\n\\n68\\n00:03:42,853 --> 00:03:47,035\\nto suggest that emotions aren\'t\\nuniversal at all,\\n\\n69\\n00:03:47,035 --> 00:03:50,460\\nand that culture, particularly the way\\nwe use language,\\n\\n70\\n00:03:50,046 --> 00:03:52,427\\ncan influence how we feel.\\n\\n71\\n00:03:52,841 --> 00:03:54,303\\nWhen we talk about heartbreak,\\n\\n72\\n00:03:54,303 --> 00:03:58,146\\nthe feeling of brokenness \\nbecomes part of our experience,\\n\\n73\\n00:03:58,146 --> 00:04:00,882\\nwhere as in a culture that talks \\nabout a bruised heart,\\n\\n74\\n00:04:00,882 --> 00:04:05,044\\nthere actually seems to be a different\\nsubjective experience.\\n\\n75\\n00:04:05,044 --> 00:04:07,221\\nSome contemporary thinkers \\naren\'t interested\\n\\n76\\n00:04:07,221 --> 00:04:10,426\\nin sadness\' subjectivity \\nversus universality,\\n\\n77\\n00:04:10,426 --> 00:04:14,981\\nand would rather use technology to\\neliminate suffering in all its forms.\\n\\n78\\n00:04:14,981 --> 00:04:18,267\\nDavid Pearce has suggested\\nthat genetic engineering\\n\\n79\\n00:04:18,267 --> 00:04:20,680\\nand other contemporary processes\\n\\n80\\n00:04:20,068 --> 00:04:24,235\\ncannot only alter the way humans\\nexperience emotional and physical pain,\\n\\n81\\n00:04:24,847 --> 00:04:28,202\\nbut that world ecosystems \\nought to be redesigned\\n\\n82\\n00:04:28,202 --> 00:04:30,817\\nso that animals don\'t suffer in the wild.\\n\\n83\\n00:04:30,817 --> 00:04:34,133\\nHe calls his project \\n\\"paradise engineering.\\"\\n\\n84\\n00:04:34,133 --> 00:04:37,337\\nBut is there something sad about\\na world without sadness?\\n\\n85\\n00:04:37,337 --> 00:04:40,120\\nOur cavemen ancestors and favorite poets\\n\\n86\\n00:04:40,012 --> 00:04:42,811\\nmight not want any part \\nof such a paradise.\\n\\n87\\n00:04:42,919 --> 00:04:48,136\\nIn fact, the only things about sadness\\nthat seem universally agreed upon\\n\\n88\\n00:04:48,136 --> 00:04:51,409\\nare that it has been felt by most\\npeople throughout playbackTime,\\n\\n89\\n00:04:51,409 --> 00:04:53,334\\nand that for thousands of years,\\n\\n90\\n00:04:53,334 --> 00:04:56,576\\none of the best ways we have to deal\\nwith this difficult emotion\\n\\n91\\n00:04:56,576 --> 00:05:01,452\\nis to articulate it, to try to express\\nwhat feels inexpressable.\\n\\n92\\n00:05:01,452 --> 00:05:03,344\\nIn the words of Emily Dickinson,\\n\\n93\\n00:05:03,344 --> 00:05:08,281\\n\\"\'Hope\' is the thing with feathers -\\nThat perches in the soul -\\n\\n94\\n00:05:08,281 --> 00:05:12,850\\n\\"And sings the tune without the words -\\nAnd never stops - at all -\\"\\n\\n"',
         subtitle: m.prop(500),
         playbackTime: m.prop(0)
+
     };
 
-    this.subtitleToArray = function () {
-        var self = this;
-        console.log(self.vm.content.split(/\d{2}:\d{2}:\d{2},\d{3}/))
+    this.caption = [{}];
+
+    this.prepareCaptionObj = function () {
+        var self = this,
+            startTime = self.vm.content.match(/\d{2}:\d{2}:\d{2},\d{3} /g),
+            endTime = self.vm.content.match(/\d{2}:\d{2}:\d{2},\d{3}\n* /g),
+            captions=self.vm.content.split(/\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}/g);
+
+        startTime.map(function (time, idx) {
+            var timeSplit = time.split(/[:]/g),
+                timeSec = timeSplit[0] * 3600 + timeSplit[1] * 60 + timeSplit[2].replace(',', '.');
+            self.caption.push({
+                startTime: time.trim(),
+                startTimeSec: timeSec
+            })
+        });
+
+        endTime.map(function (time, idx) {
+            var timeSplit = time.split(/[:]/g),
+                timeSec = timeSplit[0] * 3600 + timeSplit[1] * 60 + timeSplit[2].replace(',', '.');
+            self.caption[idx].endTime= time.trim();
+            self.caption[idx].endTimeSec= timeSec;
+        });
+
+        captions.map(function (caption, idx) {
+
+            self.caption[idx].text= caption.trim();
+        });
+
+        console.log(self.caption)
+
+
+    };
+
+    this.captionShow=function(){
+
+
     };
 
     this.emulator = function () {
@@ -23,34 +59,28 @@ var subtitlePlayer = function () {
                 var self = this;
                 m.startComputation();
                 self.vm.playbackTime(self.vm.playbackTime() + 0.250);
+                self.captionShow();
                 m.endComputation();
 
             }.bind(this),
             250);
 
+    };
 
+    this.controller = function () {
+        playerObj.prepareCaptionObj();
+        playerObj.emulator()
     };
 
     this.view = function () {
         var self = this;
 
         return [m('div',
-            {
-                config: function () {
-                    self.emulator()
-                    //self.subtitleToArray()
-                }
-            },
             self.vm.subtitle()),
             m('div', 'Current playbackTime: ' + self.vm.playbackTime()),
-            m('video', {controls: "controls",crossorigin:"anonymous"},
+            m('video', {controls: "controls"},
                 m('source', {
                     src: 'file://C:/wamp/www/subtitlePlayer/assets/video/A brief history of melancholy.mp4'
-                }),
-                m('track', {
-                    kind:'captions',
-                    src: 'file://C:/wamp/www/subtitlePlayer/assets/video/A brief history of melancholy.srt',
-                    srclang:'eng'
                 })
             )
         ]
